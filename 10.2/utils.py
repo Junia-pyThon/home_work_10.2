eb65e537e61a7ae426f49a03e71805361bd40ed2
Evgenii <evrenbi4@gmail.com>
2022-07-06 19:56:20 +0300

    chto-to sdelal

--- 10.2/utils.py
+++ 10.2/utils.py
@@ -1,22 +1,31 @@
-import requests
 import json
 from setings import PATCH_CANDIDATES_JSON
+from flask import Flask
+
+app = Flask(__name__)
 
 
 def load_candidates():
+    """
+    Загружаем список кандидатов и информацию о них из файла
+    :return:
+    """
     with open(PATCH_CANDIDATES_JSON, encoding='utf-8') as file:
         candidates = json.load(file)
         return candidates
 
-
+@app.route('/')
 def get_all():
+    return 'Hello'
+
+
+@app.route('/candidates/<x>')
+def get_by_pk(pk: str):
     pass
 
 
-def get_by_pk(pk):
+@app.route('/skills/<x>')
+def get_by_skill(skill_name: str):
     pass
 
-
-def get_by_skill(skill_name):
-    pass
-
+app.run()
